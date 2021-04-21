369cae2753fab4ed54f53804aff7eba753bf7145
hypones <57056752+hypones@users.noreply.github.com>
2021-04-21 03:25:24 +0300

    Add files via upload

---
+++ I.P_Sms_Spoof.py
@@ -0,0 +1,49 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+from os import system as s
+
+
+
+banner = """
+                            
+┌───── •✧✧• ─────┐
+ IllegalPlatform 
+└───── •✧✧• ─────┘                                                                                                     
+                                                                                                                                
+|Sms Spoofing Tool - İllegalPlatform.com 
+|Keremsanti tarafından türkçeleştirildi. Tool bana ait değildir.
+|Bilgi:
+|Günlük Limitin : 1
+|Mesajını Çok Uzun Karakter Tutma
+|Telefon Numarasını +90 Şeklinde Doğru Yaz
+
+"""
+
+print(banner)
+
+sor = input("Gönderilecek Telefon Numarası : Örn:+905***** >>> ")
+
+mesaj = input("Mesajınız >>> ")
+
+arlk = mesaj[0:70]
+
+print("\n| Gönderlicek Mesajın Taslağı aşagıdaki gibidir.\n"+arlk)
+
+drlm = input("\n| Mesajın Gönderilmesini Onaylıyor musunuz ? [y/n] >>> ")
+
+if drlm == "y" or drlm == "Y":
+    print("\n"+sor+"\n"+arlk+"\n")
+    resp = requests.post('https://textbelt.com/text', {
+  'phone': sor,
+  'message': arlk,
+  'key': 'textbelt',
+    })
+    print(resp.json())
+
+elif drlm == "n" or drlm == "N":
+    quit()
+else:
+    print("\n|Hatalı Format.")
+
